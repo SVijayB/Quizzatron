a464a68ea67cc39497c2158d8826e738cca4ef64
svijayb <svijayb.dev@gmail.com>
2025-02-25 02:50:59 -0800

    Updated image module to serve static images to be rendered on the front-end

--- api/utils/extract_img.py
+++ api/utils/extract_img.py
@@ -1,43 +1,43 @@
 from icrawler.builtin import GoogleImageCrawler
+from flask import request
 import logging
 import os
 import glob
 
-# Supressing all the icrawler logs
 logging.getLogger("icrawler").setLevel(logging.CRITICAL)
 logging.getLogger("feeder").setLevel(logging.CRITICAL)
 logging.getLogger("parser").setLevel(logging.CRITICAL)
 logging.getLogger("downloader").setLevel(logging.CRITICAL)
 
+TEMP_FOLDER = "api/static/temp"
+os.makedirs(TEMP_FOLDER, exist_ok=True)
+
 
 def cleanup_temp_folder():
-    temp_folder = "temp"
-    for image_file in glob.glob(os.path.join(temp_folder, "*")):
+    """Remove all images from the temp folder."""
+    for image_file in glob.glob(os.path.join(TEMP_FOLDER, "*")):
         os.remove(image_file)
     logging.info("🧹 Clean-up activity completed.")
 
 
 def download_images(query):
-    filters = {
-        "size": "medium",
-        "license": "noncommercial",
-    }
-    google_crawler = GoogleImageCrawler(storage={"root_dir": "temp"})
+    filters = {"size": "medium", "license": "noncommercial"}
+    google_crawler = GoogleImageCrawler(storage={"root_dir": TEMP_FOLDER})
     google_crawler.crawl(keyword=query, max_num=1, filters=filters)
 
-    downloaded_images = glob.glob(os.path.join("temp", "000001*"))
+    downloaded_images = glob.glob(os.path.join(TEMP_FOLDER, "000001*"))
     if downloaded_images:
         original_image_path = downloaded_images[0]
-        query = query.replace(" ", "_")
-        new_image_path = os.path.join("temp", f"{query}.jpg")
+        sanitized_query = query.replace(" ", "_") + ".jpg"
+        new_image_path = os.path.join(TEMP_FOLDER, sanitized_query)
 
         if os.path.exists(new_image_path):
             os.remove(new_image_path)
         os.rename(original_image_path, new_image_path)
-        path = new_image_path
+
         logging.info(f"📸 {query} downloaded successfully.")
+        base_url = request.host_url.rstrip("/")
+        return f"{base_url}/static/temp/{sanitized_query}"
     else:
         logging.warning(f"No image found for query: {query}")
-        path = None
-
-    return path
+        return None
