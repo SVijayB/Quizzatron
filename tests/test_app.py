789e18cecbbe5f6b5dab5caad310564f43ece4f4
svijayb <svijayb.dev@gmail.com>
2025-03-13 18:45:27 -0700

    Updated app creation for testing (fixes build)

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from api.app import create_app
 
 @pytest.fixture
 def app():
-    app = create_app()
+    app = create_app("testing")
     app.config.update(
         {
             "TESTING": True,
