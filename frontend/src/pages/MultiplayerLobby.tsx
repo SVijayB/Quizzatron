d180d6137e7c796196e38a6badd73e59704a1232
svijayb <svijayb.dev@gmail.com>
2025-05-01 18:03:56 -0700

    Updated backend endpoints

--- frontend/src/pages/MultiplayerLobby.tsx
+++ frontend/src/pages/MultiplayerLobby.tsx
@@ -225,7 +225,7 @@ const MultiplayerLobby = () => {
   
   const fetchCategories = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/categories/get');
+      const response = await fetch('https://quizzatron.onrender.com/api/categories/get');
       if (!response.ok) {
         throw new Error('Failed to fetch categories');
       }
